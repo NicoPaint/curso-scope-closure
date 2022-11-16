d64ec3d02cc38588ca720afe383e39857af8324c
Nicolas Pinto <felipepinve@gmail.com>
2022-11-16 10:25:30 -0600

    Se vio el modo estricto y  su uso con algunos ejemplos

---
+++ src/scope/Strict.js
@@ -0,0 +1,13 @@
+"use strict";
+pi = 3.1416;
+console.log(pi);
+
+function myFunction(){
+    "use strict";
+    pi = 3.1416;
+    return pi;
+}
+
+console.log(myFunction());
+
+//en el modo estricto se aplican mas reglas al lenguaje de programcion como por ejemplo tener que declarar una variable primero de inicializarla
